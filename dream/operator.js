1af666bef648a6a8870b1624dbb2b4fba235ace9
rlagnswo0505 <rlagnswo0505@naver.com>
2022-04-30 19:00:24 +0900

    update operator.js

--- dream/operator.js
+++ dream/operator.js
@@ -1,4 +1,26 @@
 // 1. String concatenation
 console.log('my' + ' cat');
 console.log('1' + 2);
-console.log();
+console.log(`string literals: 
+,,,,
+1 + 2 = ${1 + 2}`);
+console.log("ellie's \n\tbook");
+
+// 2. Numeric operators
+console.log(1 + 1); // add
+console.log(1 - 1); // subtract
+console.log(1 / 1); // divide
+console.log(1 * 1); // multiply
+console.log(5 % 2); // remainder
+console.log(2 ** 3); // exponentiation
+
+// 3. Increment and decrement operators
+let counter = 2;
+const preIncrement = ++counter;
+// counter = counter + 1;
+// preIncrement = counter;
+console.log(`preIncrement : ${preIncrement}, counter : ${counter}`);
+const postIncrement = counter++;
+// postIncrement = counter;
+// counter = counter + 1;
+console.log(`postIncrement : ${postIncrement}, counter : ${counter}`);
