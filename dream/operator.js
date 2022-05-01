b338378f70202871a83573cd3bf8a84368037c5e
rlagnswo0505 <rlagnswo0505@naver.com>
2022-05-01 16:59:49 +0900

    equality puzzler

--- dream/operator.js
+++ dream/operator.js
@@ -75,3 +75,19 @@ console.log(stringFive != numberFive);
 // === strict equality, no type conversion
 console.log(stringFive === numberFive);
 console.log(stringFive !== numberFive);
+
+// object equality by reference
+const ellie1 = { name: 'ellie' };
+const ellie2 = { name: 'ellie' };
+const ellie3 = ellie1;
+console.log(ellie1 == ellie2);
+console.log(ellie1 === ellie2);
+console.log(ellie1 === ellie3);
+
+// equality - puzzler
+console.log(0 == false);
+console.log(0 === false);
+console.log('' == false);
+console.log('' === false);
+console.log(null == undefined);
+console.log(null === undefined);
