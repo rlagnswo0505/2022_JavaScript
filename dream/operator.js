8d95ab52446a92e669832e5a5c28ce788982711a
rlagnswo0505 <rlagnswo0505@naver.com>
2022-05-01 17:14:22 +0900

    break continue

--- dream/operator.js
+++ dream/operator.js
@@ -133,3 +133,44 @@ while (i > 0) {
     console.log(`while: ${i}`);
     i--;
 }
+
+// do while loop, body code is executed first,
+// then check the condition.
+do {
+    console.log(`do while: ${i}`);
+    i--;
+} while (i > 0);
+
+// for loop, for(begin; condition; step)
+for (i = 3; i > 0; i--) {
+    console.log(`for: ${i}`);
+}
+
+for (let i = 3; i > 0; i = i - 2) {
+    // inline variable declaration
+    console.log(`inline variable for: ${i}`);
+}
+
+// nested loops
+for (let i = 0; i < 10; i++) {
+    for (let j = 0; j < 10; j++) {
+        console.log(`i: ${i}, j:${j}`);
+    }
+}
+
+// break, continue
+// Q1. iterate from 0 to 10 and print only even numbers (use continue)
+for (let i = 0; i < 11; i++) {
+    if (i % 2 === 0) {
+        continue;
+    }
+    console.log(`q1. ${i}`);
+}
+
+// Q2. iterate from 0 to 10 and print numbers until reaching 8 (use break)
+for (let i = 0; i < 11; i++) {
+    if (i > 8) {
+        break;
+    }
+    console.log(`q2. ${i}`);
+}
