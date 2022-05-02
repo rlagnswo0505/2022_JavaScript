fb9a74b64ae8eb0a76261def4bc7502461e01f7c
rlagnswo0505 <rlagnswo0505@naver.com>
2022-05-02 18:07:42 +0900

    update arrowfunction

--- dream/class.js
+++ dream/class.js
@@ -74,3 +74,27 @@ const article1 = new Article(1);
 const article2 = new Article(2);
 console.log(Article.publisher);
 Article.printPublisher();
+
+// 5. Ingeritance
+// a way for one class to extend anther class.
+class Shape {
+    constructor(width, height, color) {
+        this.width = width;
+        this.height = height;
+        this.color = color;
+    }
+    draw() {
+        console.log(`drawing ${this.color} color of`);
+    }
+
+    getArea() {
+        return width * this.height;
+    }
+}
+
+class Rectangle extends Shape { }
+class Triangle extends shape { }
+const rectangle = new Rectangle(20,20,'blue');
+rectangle.draw();
+const triangle = new Triangle(20,20,'blue');
+triangle.draw();
---
+++ dream/foreach.js
@@ -0,0 +1,6 @@
+const newArr2 = [1, 2, 3, 4];
+
+
+newArr2.forEach(function (element) {
+    return console.log(element);
+});
