89a4ff1d20b7e3b0e8351a17aa54c1f4e8d86540
rlagnswo0505 <rlagnswo0505@naver.com>
2022-05-10 16:37:48 +0900

    create async

---
+++ ch03/async.js
@@ -0,0 +1,6 @@
+console.log('A');
+setTimeout(function () {
+    console.log('B');
+},1000)
+console.log('C');
+console.log('D');
--- ch03/obj.js
+++ ch03/obj.js
@@ -1,4 +1,4 @@
-// 객체생성용 함수는 대문자로 시작
+// 객체생성용 함수는 대문자로 시작 [생성자함수]
 function Human(name, age) {
     // 속성
     this.name = name;
@@ -10,7 +10,17 @@ function Human(name, age) {
     }
 }
 var h1 = new Human('홍길동', 20);
+// 객체에 함수를 추가해서 쓸 수 있다.
+Human.prototype.hello = function () {
+    console.log(`Hello ${this.name}`);
+}
+h1.height = 180;
 var h2 = new Human('둘리', 120);
 
 h1.showMe();
 h2.showMe();
+h1.hello();
+h2.hello();
+
+console.log(`h1.height : ${h1.height}`);
+console.log(`h2.height : ${h2.height}`);
