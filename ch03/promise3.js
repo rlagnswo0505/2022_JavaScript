406453cc82199d5a170cac1b48a3cdbd4b463b81
rlagnswo0505 <rlagnswo0505@naver.com>
2022-05-10 17:10:38 +0900

    update promise3

---
+++ ch03/promise3.js
@@ -0,0 +1,19 @@
+function fn1() {
+    return new Promise(function (resolve) {
+        setTimeout(function () {
+            resolve(10);
+        }, 2000);
+    });
+}
+function fn2(val) {
+    return new Promise(function (resolve) {
+        setTimeout(function () {
+            resolve(val + val);
+        },1000)
+    })
+}
+var p1 = fn1();
+p1.then(function (result) {
+    console.log(result);
+})
+fn2(val)
